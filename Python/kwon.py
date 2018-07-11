a3ca0b9706eee2545bfa56794d94013e4a988670
Oyun <oyoon33@gmail.com>
2018-07-11 22:38:37 +0900

    Add files via upload

--- Python/kwon.py
+++ Python/kwon.py
@@ -1,5 +1,13 @@
-fhand = open('C:/Users/oyun/Desktop/kwon.txt', 'r')
-for lx in fhand:
-    ly = lx.rstrip()
-    print(ly)
-
+fhand = open('C:/Users/oyun/Desktop/kwon.txt')
+di = dict()
+for line in fhand:
+    line = line.rstrip()
+    wds = line.split()
+    for w in wds:
+        di[w] = di.get(w,0)+1
+largest = 0
+for a,b in di.items():
+    if b>largest:
+        largest = b
+    print(a, b)
+print('가장큰 값은', largest)
