56ef8ece89e7e54d4fc0f503be6b983da65b953c
Oyun <oyoon33@gmail.com>
2018-08-11 16:01:40 +0900

    Add files via upload

--- fitbitdata.py
+++ fitbitdata.py
@@ -1,9 +1,69 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import xlrd
 
-train_url = "http://s3.amazonaws.com/assets.datacamp.com/course/Kaggle/train.csv"
-train = pd.read_csv(train_url)
+SleepData  = pd.read_excel('C:/Users/oyun/Desktop/fitbitdata.xls', sheet_name='수면')
+ActiveData = pd.read_excel('C:/Users/oyun/Desktop/fitbitdata.xls', sheet_name='활동')
 
-test_url = "http://s3.amazonaws.com/assets.datacamp.com/course/Kaggle/test.csv"
-test = pd.read_csv(test_url)
+date = ActiveData['날짜']
 
-print(train.shape)
+kal = np.array(ActiveData['칼로리 소모량'])
+for i in range(len(kal)):
+    kal[i] = int(kal[i].replace(",",""))
+
+step = np.array(ActiveData['걸음 수'])
+for i in range(len(step)):
+    step[i] = int(step[i].replace(",", ""))
+
+distance = np.array(ActiveData['이동 거리'])
+
+activetime = ActiveData['약간 활동적인 시간(분)']
+
+a = np.array([[1,2,3,4]])
+b = np.array([5,6,7,8])
+print(b)
+print(b.ndim)
+print(b.shape)
+b = b.reshape(1,-1)
+print(b[b<8])
+b = np.where(b<7, 100, 200)
+print(b.shape)
+print(b.ndim)
+print(b)
+print(b.flatten())
+c = np.zeros(10,)
+c = c+1
+print(c)
+# print(kal.dtype)
+# print(distance.dtype)
+
+def extract_date(column):
+    return column.split(".")[2]
+
+extractdate = date.apply(extract_date)
+
+plt.subplot(2,2,1)
+plt.plot(extractdate, kal)
+plt.title("Calorie for Date")
+plt.xlabel("Date")
+plt.ylabel("Calorie")
+
+plt.subplot(2,2,2)
+plt.plot(extractdate, activetime)
+plt.title("Active time for Date")
+plt.xlabel("Date")
+plt.ylabel("Active Time")
+
+plt.subplot(2,2,3)
+plt.plot(extractdate, step)
+plt.title("Step for Date")
+plt.xlabel("Date")
+plt.ylabel("Steps")
+
+plt.subplot(2,2,4)
+plt.plot(extractdate, distance)
+plt.title("Distance for Date")
+plt.xlabel("Date")
+plt.ylabel("Distance")
+# plt.show()
